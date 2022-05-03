8a2be7de9cbcf5889542d6b9ac856bc8349b628c
Edmundo Paulino <manyymoore@gmail.com>
2022-05-03 06:12:17 -0700

    feat: ✨ Copy the password to the clipboard
    By default, program places generated password onto the clipboard
    
    This behaviour can be changed with a flag

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use arboard::Clipboard;
 use pwgen::generate;
 use structopt::StructOpt;
 
@@ -12,10 +13,29 @@ struct Opt {
     /// Set password length [max: 255]
     #[structopt(short, long, default_value = "16")]
     length: u8,
+
+    /// Do not not copy the password to the clipboard
+    #[structopt(long = "no-copy")]
+    do_not_copy: bool,
 }
 
 fn main() {
     let opt = Opt::from_args();
 
-    println!("{}", generate(opt.r#type.as_str(), opt.length));
+    let pw = generate(opt.r#type.as_str(), opt.length);
+    println!("{}", &pw);
+
+    if !opt.do_not_copy {
+        copy(pw);
+    }
+}
+
+fn copy(pw: String) {
+    if let Ok(mut clipboard) = Clipboard::new() {
+        if clipboard.set_text(pw).is_err() {
+            panic!("Could not place the password onto the clipboard");
+        }
+    } else {
+        panic!("Could not create an instance of the clipboard");
+    }
 }
